4e303b02ed3a92277db2227856729544ec1c72c1
Arithmetic-Power-Geometry <akhtarneha310@gmail.com>
2026-09-16 12:52:45 +0530

    Fix SAFESEP-EXISTS family imports

--- tests/test_existence.py
+++ tests/test_existence.py
@@ -3,7 +3,7 @@ from math import inf
 
 from safesep.existence import safesep_exists
 from safesep.examples import minimal_deadlock_problem, adaptive_safe_problem, already_homogeneous_problem
-from safesep.irreducibility import parameterized_pair
+from safesep.families import finite_family, infinite_family
 from safesep.solver import solve_safesep
 
 
@@ -15,6 +15,5 @@ def test_existence_matches_optimization_on_core_cases():
 
 def test_existence_separates_parameterized_family():
     for n in range(2, 31):
-        a, b = parameterized_pair(n)
-        assert safesep_exists(a)
-        assert not safesep_exists(b)
+        assert safesep_exists(finite_family(n))
+        assert not safesep_exists(infinite_family(n))
